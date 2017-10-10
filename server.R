f7e3a983cf9355030c2baa0130aa8a8c59f7c358
Tara Cooper <coopert19@mail.wlu.edu>
2017-10-10 13:08:25 -0400

    edited to add ggplot function shell

--- server.R
+++ server.R
@@ -1,1 +1,15 @@
 library(shiny)
+library(tidyverse)
+state_specialty <- read.table("specialty_by_state_data.csv", header = TRUE, sep = ',')
+
+# Define server logic required to draw a histogram
+function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    
+    # draw the barplot with the specialties as x
+    ggplot("state_specialty", aes())
+    
+  })
+  
+}
