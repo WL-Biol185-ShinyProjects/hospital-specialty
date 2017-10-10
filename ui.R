64a47f293bb44f30c8ff73afec1ade9b8522dcd5
Tara Cooper <coopert19@mail.wlu.edu>
2017-10-10 12:56:21 -0400

    Replaced slide with select box

--- ui.R
+++ ui.R
@@ -1,1 +1,27 @@
 library(shiny)
+
+# Define UI for application that draws a histogram
+fluidPage(
+  
+  # Application title
+  titlePanel("Physician Specialty by State Data"),
+  
+  # Sidebar with a input for selection of states
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("state",
+                  "State:",
+                  state_specialty$Location,
+                  selected = NULL,
+                  multiple = TRUE,
+                  selectize = TRUE,
+                  width = NULL,
+                  size = NULL)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
