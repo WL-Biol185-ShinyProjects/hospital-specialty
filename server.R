13195f956e184859d69577a6c86ad817a245b1ab
Tara Cooper <coopert19@mail.wlu.edu>
2017-10-17 13:15:31 -0400

    figured out how to gather

--- server.R
+++ server.R
@@ -5,10 +5,10 @@ state_specialty <- read.table("specialty_by_state_data.csv", header = TRUE, sep 
 # Define server logic required to draw a histogram
 function(input, output) {
   
-  output$distPlot <- renderPlot({
+  output$specialty_plot <- renderPlot({
     
     # draw the barplot with the specialties as x
-    ggplot("state_specialty", aes())
+    gather(state_specialty, key = "specialty", value = "value", Psychiatry:Other)
     
   })
   
--- ui.R
+++ ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+state_specialty <- read.table("specialty_by_state_data.csv", header = TRUE, sep = ',')
 
 # Define UI for application that draws a histogram
 fluidPage(
@@ -9,8 +10,8 @@ fluidPage(
   # Sidebar with a input for selection of states
   sidebarLayout(
     sidebarPanel(
-      selectInput("state",
-                  "State:",
+      selectInput( inputId = 'state',
+                  label = 'Select a State',
                   state_specialty$Location,
                   selected = NULL,
                   multiple = TRUE,
@@ -21,7 +22,7 @@ fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("specialty_plot")
     )
   )
 )
