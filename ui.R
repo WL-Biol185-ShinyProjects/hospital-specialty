49a415544c05e35c17a21efbfbce87c18de6fe2d
Gabe <witherellg19@mail.wlu.edu>
2017-11-09 12:19:36 -0500

    Added the beginning of a dashboard

--- ui.R
+++ ui.R
@@ -1,28 +1,34 @@
 library(shiny)
+library(shinydashboard)
 state_specialty <- read.table("specialty_by_state_data.csv", header = TRUE, sep = ',')
 
-# Define UI for application that draws a histogram
-fluidPage(
-  
-  # Application title
-  titlePanel("Physician Specialty by State Data"),
-  
-  # Sidebar with a input for selection of states
-  sidebarLayout(
-    sidebarPanel(
-      selectInput( inputId = 'state',
-                  label = 'Select a State',
-                  state_specialty$Location,
-                  selected = "United States",
-                  multiple = FALSE,
-                  selectize = FALSE,
-                  width = NULL,
-                  size = NULL)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("specialty_plot")
-    )
+#Define UI for application that hosts all fluidpages
+dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    # Define UI for application that draws a histogram
+    fluidPage(
+      
+      # Application title
+      titlePanel("Physician Specialty by State Data"),
+      
+      # Sidebar with a input for selection of states
+      sidebarLayout(
+        sidebarPanel(
+          selectInput( inputId = 'state',
+                       label = 'Select a State',
+                       state_specialty$Location,
+                       selected = "United States",
+                       multiple = FALSE,
+                       selectize = FALSE,
+                       width = NULL,
+                       size = NULL)
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+          plotOutput("specialty_plot")
+        )
   )
-)
+)))
